3ee49072ad9762fc90808ee2f9a1b5c12831c801
theolevison <theo.levison@gmail.com>
2023-11-01 16:42:39 +0000

    added Person class with some utility methods

---
+++ src/main/java/Person.java
@@ -0,0 +1,21 @@
+
+public class Person {
+	private String name;
+	private int id;
+	
+	public void getStyleAttributes() {
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+}
