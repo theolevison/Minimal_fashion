a076fda44691f24f8221668a393bef0a5f7712ae
theolevison <theo.levison@gmail.com>
2023-11-18 12:11:18 +0000

    added spring

--- src/main/java/com/theo/minimal_fashion/Controller.java
+++ src/main/java/com/theo/minimal_fashion/Controller.java
@@ -1,40 +1,47 @@
 package com.theo.minimal_fashion;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@SpringBootApplication
+@RestController
 public class Controller {
 	private Person person;
 	
 	public static void main(String[] args) {
-		if (args.length > 0) {
-			if (args[0].equals("test")) {
-				//do nothing because we have junit
-				System.out.println("why do you have args");
-			}
-		} else {
-			//do stuff
-			System.out.println("started");
-			
-			DynamoDBAbstraction db = new DynamoDBAbstraction();
-			//db.get("tshirt", "Torso");
-			
-			//create object to put in table
-			//TODO: use a factory method to bring back Torso, Legs etc
-			TorsoClothingFactory tcf = new TorsoClothingFactory();
-			Clothing tshirt = tcf.setupClothing("red tshirt");
-			
-			//all optional
-			/**
-			tshirt.setCondition(10);
-			tshirt.setLayerable(true);
-			tshirt.setStyleRating(0);
-			tshirt.setTemperatureRating(5);
-			tshirt.testMethod();
-			*/
-			
-			db.put(tshirt);
-			db.superGet("Torso", "red tshirt");
-			//Clothing testShirt = db.get("Torso", "red tshirt");
-		}
+		SpringApplication.run(Controller.class, args);
+		
+		//do stuff
+		System.out.println("started");
+		
+		DynamoDBAbstraction db = new DynamoDBAbstraction();
+		//db.get("tshirt", "Torso");
+		
+		//create object to put in table
+		//TODO: use a factory method to bring back Torso, Legs etc
+		TorsoClothingFactory tcf = new TorsoClothingFactory();
+		Clothing tshirt = tcf.setupClothing("red tshirt");
+		
+		//all optional
+		/**
+		tshirt.setCondition(10);
+		tshirt.setLayerable(true);
+		tshirt.setStyleRating(0);
+		tshirt.setTemperatureRating(5);
+		tshirt.testMethod();
+		*/
+		
+		db.put(tshirt);
+		db.superGet("Torso", "red tshirt");
+		//Clothing testShirt = db.get("Torso", "red tshirt");
 	}
+	
+	@GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+      return String.format("Hello %s!", name);
+    }
 	
 	public void addPerson(String name) {
 		if (person.equals(null)) {
